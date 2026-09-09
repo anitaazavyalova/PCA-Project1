902e2bd0e6fe8155094a79fb3013573b4b3eb19f
anitaazavyalova <m287116@usna.edu>
2026-09-08 20:31:48 -0400

    Add files via upload

--- PCAProjectUPDATED.R
+++ PCAProjectUPDATED.R
@@ -17,15 +17,18 @@ data_std=scale(my_data)
 cov_matrix = cov(data_std)
 cov_matrix
 
+#This is where we get the eigenvalues and vectors for the data
 eigen_decomp = eigen(cov_matrix)
 Eigenvalues = eigen_decomp$values
 Eigenvectors = eigen_decomp$vectors
 
+#Eigen-proportion
 Eigenprop = Eigenvalues /sum(Eigenvalues)
 round(Eigenprop,3)
 
 Eigenvectors
 
+#Loadings are the coefficients that give linear combinations from the original data
 loadings = Eigenvectors %>% 
   data.frame(row.names = colnames(my_data)) %>%
   rename("PC1" = X1, "PC2" = X2, "PC3" = X3, "PC4" = X4) %>%
@@ -33,8 +36,11 @@ loadings = Eigenvectors %>%
 
 loadings
 
+#Proportion of Variance Explained
+#Captures total variance within the dataset.
+
 pve = 100 * Eigenprop / sum(Eigenprop)
-# this can be much more informative by looking at plots
+# We can look at a plot to get the percentages of variance/unique values in the dataset
 par(mfrow = c(1, 2))
 plot(pve, type = "b", ylab = "PVE",
      xlab = "Principal Component", col = "blue")
@@ -45,7 +51,7 @@ plot(cumsum(pve), type = "b", ylab = "Cumulative PVE",
 PCscores = data_std %*% Eigenvectors 
 head(PCscores)
 
-# PC scores are orthogonal to (i.e., uncorrelated with) each other:
+# PC scores are orthogonal to each other:
 cor(PCscores)
 
 biplot(PCscores[,1:2], loadings[,1:2], 
