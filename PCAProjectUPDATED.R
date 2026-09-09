8470210ec030d92ead05843bf575a4c5926e0f90
Jovanni-Garces <m282196@usna.edu>
2026-09-08 21:25:13 -0400

    Trying to find biggest loadings.

--- PCAProjectUPDATED.R
+++ PCAProjectUPDATED.R
@@ -11,34 +11,26 @@ head(my_data)
 apply(my_data, 2, mean)
 apply(my_data, 2, var)
 
-# Standardizing the data, the computing the covariance matrix
+# Standardizing the data, then computing the covariance matrix
 data_std=scale(my_data)
 
 cov_matrix = cov(data_std)
 cov_matrix
 
-<<<<<<< HEAD
 # Computing eigenvalues and vectors to be able to see how the variables
 # move and at what scale
-=======
-#This is where we get the eigenvalues and vectors for the data
->>>>>>> 902e2bd0e6fe8155094a79fb3013573b4b3eb19f
 eigen_decomp = eigen(cov_matrix)
 Eigenvalues = eigen_decomp$values
 Eigenvectors = eigen_decomp$vectors
 
 #Eigen-proportion
-Eigenprop = Eigenvalues /sum(Eigenvalues)
+Eigenprop = Eigenvalues/sum(Eigenvalues)
 round(Eigenprop,3)
 
 Eigenvectors
 
-<<<<<<< HEAD
 # Computing loadings, which is the weight at which variables 
 # contribute to a PC 
-=======
-#Loadings are the coefficients that give linear combinations from the original data
->>>>>>> 902e2bd0e6fe8155094a79fb3013573b4b3eb19f
 loadings = Eigenvectors %>% 
   data.frame(row.names = colnames(my_data)) %>%
   rename("PC1" = X1, "PC2" = X2, "PC3" = X3, "PC4" = X4,
@@ -46,10 +38,15 @@ loadings = Eigenvectors %>%
          "PC9" = X9, "PC10" = X10, "PC11" = X11, "PC12" = X12,
          "PC13" = X13, "PC14" = X14, "PC15" = X15, "PC16" = X16) %>%
   round(digits = 3)
-
 loadings
 
-<<<<<<< HEAD
+
+# Since loadings are the "weights", the rows with the highest magnitudes are
+# target/most influential variables 
+
+loadings[order(abs(loadings$PC1, decreasing = True) ]
+
+
 # Computing the Proportion of Variance which is the percentage of 
 # the original MSTAR data that is retained by each principal component. 
 
@@ -61,13 +58,11 @@ min(which(cumsum(pve) >= 85))
 
 
 # Creating plots for visibility
-=======
-#Proportion of Variance Explained
-#Captures total variance within the dataset.
+
 
 pve = 100 * Eigenprop / sum(Eigenprop)
 # We can look at a plot to get the percentages of variance/unique values in the dataset
->>>>>>> 902e2bd0e6fe8155094a79fb3013573b4b3eb19f
+
 par(mfrow = c(1, 2))
 plot(pve, type = "b", ylab = "PVE",
      xlab = "Principal Component", col = "blue")
